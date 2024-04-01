156c0ef06e6730a1685b77f31c3d30268402c016
Gabriela França <gabifmouraroc@outlook.com>
2024-04-01 09:39:19 -0300

    Suspense API no React

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
+import { Suspense } from 'react'
+
+import { LongWaitComponent } from '@/components/long-wait-component'
+import GithubProfile from '@/components/github-profile'
+
 export default async function Home() {
-  await new Promise((resolve) => setTimeout(resolve, 2000))
+  return (
+    <div>
+      <h1>Home</h1>
 
-  const response = await fetch('https:api.github.com/users/gabifrancamr')
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste quos nulla
+        laudantium, provident distinctio sapiente neque quis blanditiis,
+        similique dolore ab natus nobis veniam perspiciatis sint minima expedita
+        velit eveniet.
+      </p>
+      <Suspense fallback={<p>Carregando LongWaitComponent</p>}>
+        <LongWaitComponent />
+      </Suspense>
 
-  const user = await response.json()
-
-  return <pre>{JSON.stringify(user, null, 2)}</pre>
+      <Suspense fallback={<p>Carregando GithubProfile</p>}>
+        <GithubProfile />
+      </Suspense>
+    </div>
+  )
 }

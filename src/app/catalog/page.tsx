e85de74093cecf47e43c521e0c2ee7bcf80ccf26
Gabriela França <gabifmouraroc@outlook.com>
2024-04-01 08:53:57 -0300

    react server components, fetch e loading

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,3 +1,4 @@
-export default function Catalog() {
+export default async function Catalog() {
+  await new Promise((resolve) => setTimeout(resolve, 2000))
   return <h1>Catalog</h1>
 }
--- src/app/catalog/product/[...data]/page.tsx
+++ src/app/catalog/product/[...data]/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 interface ProductsProps {
   params: {
     data: string[]
@@ -6,11 +8,20 @@ interface ProductsProps {
 
 export default function Products({ params }: ProductsProps) {
   const [productId, size, color] = params.data
+
+  console.log(params)
+
+  function addToCart() {
+    console.log('adicionar ao carrinho')
+  }
+
   return (
     <div>
       <p>Product: {productId}</p>
       <p>Size: {size}</p>
       <p>Color: {color}</p>
+
+      <button onClick={addToCart}>Adicionar ao carrinho</button>
     </div>
   )
 }

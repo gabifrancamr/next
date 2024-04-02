21d972eb7d89b07232b597760b77ae7369cceff7
Gabriela França <gabifmouraroc@outlook.com>
2024-04-02 12:03:47 -0300

    responsividade!

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,8 +1,71 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
 export default async function Home() {
-  await new Promise((resolve) => setTimeout(resolve, 2000))
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="grid max-h-[860px] md:grid-cols-9 grid-rows-6 gap-6">
+      <Link
+        href="/"
+        className="group relative col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end "
+      >
+        <Image
+          src="/moletom-never-stop-learning.png"
+          width={600}
+          height={600}
+          quality={100}
+          alt=""
+          className="group-hover:scale-105 transition-transform duration-500"
+        />
+
+        <div className="absolute bottom-12 md:bottom-28 right-21 h-12 flex items-center gap-2 max-width-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span className="text-sm truncate">Moletom never stop learning</span>
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+            R$129
+          </span>
+        </div>
+      </Link>
+
+      <Link
+        href="/"
+        className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end "
+      >
+        <Image
+          src="/moletom-java.png"
+          width={280}
+          height={280}
+          quality={100}
+          alt=""
+          className="group-hover:scale-105 transition-transform duration-500"
+        />
+
+        <div className="absolute bottom-4 md:bottom-8 right-15 h-8 md:h-12 flex items-center gap-2 max-width-[50px] md:max-width-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-1 md:pl-5">
+          <span className="text-xs md:text-sm truncate">Moletom Java</span>
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-2 md:px-4 text-xs md:text-base font-semibold">
+            R$129
+          </span>
+        </div>
+      </Link>
+
+      <Link
+        href="/"
+        className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end "
+      >
+        <Image
+          src="/camiseta-dowhile.png"
+          width={280}
+          height={280}
+          quality={100}
+          alt=""
+          className="group-hover:scale-105 transition-transform duration-500"
+        />
+
+        <div className="absolute bottom-4 md:bottom-8 right-15 h-8 md:h-12 flex items-center gap-2 max-width-[50px] md:max-width-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-1 md:pl-5">
+          <span className="text-xs md:text-sm truncate">Camiseta do-while</span>
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-2 md:px-4 text-xs md:text-base font-semibold">
+            R$129
+          </span>
+        </div>
+      </Link>
     </div>
   )
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,12 +5,15 @@ import Image from 'next/image'
 export function Header() {
   return (
     <div className="flex items-center justify-between">
-      <div className="flex items-center gap-5">
-        <Link href="/" className="text-2xl font-extrabold text-white">
+      <div className="flex items-center gap-3 md:gap-5">
+        <Link
+          href="/"
+          className="text-xl md:text-2xl font-extrabold text-white"
+        >
           devstore
         </Link>
 
-        <form className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700">
+        <form className="flex w-[200px] md:w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700">
           <Search className="w-5 h-5 text-zinc-500" />
 
           <input
@@ -23,13 +26,13 @@ export function Header() {
       <div className="flex items-center gap-4">
         <div className="flex items-center gap-2">
           <ShoppingBag className="h-4 w-4" />
-          <span className="text-sm">Cart (0)</span>
+          <span className="hidden md:block text-sm">Cart (0)</span>
         </div>
 
         <div className="w-px h-4 bg-zinc-700"></div>
 
         <Link href="/" className="flex items-center gap-2 hover:underline">
-          <span className="text-sm">Account</span>
+          <span className="hidden md:block text-sm">Account</span>
           <Image
             src="https://github.com/gabifrancamr.png"
             alt="foto de perfil"

8395ba93909b284740141f141d771b7925f0ad3b
Gabriela França <gabifmouraroc@outlook.com>
2024-04-02 09:14:32 -0300

    criando estrutura do app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,7 @@
-import { Suspense } from 'react'
-
-import { LongWaitComponent } from '@/components/long-wait-component'
-import GithubProfile from '@/components/github-profile'
-
 export default async function Home() {
   return (
     <div>
-      <h1>Home</h1>
-
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste quos nulla
-        laudantium, provident distinctio sapiente neque quis blanditiis,
-        similique dolore ab natus nobis veniam perspiciatis sint minima expedita
-        velit eveniet.
-      </p>
-      <Suspense fallback={<p>Carregando LongWaitComponent</p>}>
-        <LongWaitComponent />
-      </Suspense>
-
-      <Suspense fallback={<p>Carregando GithubProfile</p>}>
-        <GithubProfile />
-      </Suspense>
+      <h1>Hello World</h1>
     </div>
   )
 }

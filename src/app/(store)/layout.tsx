190e3d313b2f3bb40f870352b1db9a3b213b840e
Gabriela França <gabifmouraroc@outlook.com>
2024-04-05 09:00:38 -0300

    Contexto do carrinho

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,11 +1,14 @@
+import { CartProvider } from '@/components/card-context'
 import { Header } from '@/components/header'
 import { ReactNode } from 'react'
 
 export default function StoreLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-app gap-5 p-2 md:p-8">
-      <Header />
-      {children}
-    </div>
+    <CartProvider>
+      <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-app gap-5 p-2 md:p-8">
+        <Header />
+        {children}
+      </div>
+    </CartProvider>
   )
 }

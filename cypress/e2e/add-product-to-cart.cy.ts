c0afed50059e8cb8c9484215829022d9292e77ac
Gabriela França <gabifmouraroc@outlook.com>
2024-04-09 10:51:32 -0300

    testando a busca

--- cypress/e2e/add-product-to-cart.cy.ts
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -1,7 +1,9 @@
 describe('add product to cart', () => {
-  it('must be able to navigate to the product page and add it to the cart', () => {
-    cy.visit('http://localhost:3000')
+  beforeEach(() => {
+    cy.visit('/')
+  })
 
+  it('must be able to navigate to the product page and add it to the cart', () => {
     cy.get('a[href^="/product"]').first().click()
 
     // cy.url().should('include', '/product')
@@ -13,8 +15,6 @@ describe('add product to cart', () => {
   })
 
   it('should not count duplicated products on cart', () => {
-    cy.visit('http://localhost:3000')
-
     cy.get('a[href^="/product"]').first().click()
 
     // cy.url().should('include', '/product')
@@ -27,8 +27,6 @@ describe('add product to cart', () => {
   })
 
   it('should be able to search for a product and add it to the cart', () => {
-    cy.visit('http://localhost:3000')
-
     cy.get('input[name=q]').type('moletom').parent('form').submit()
 
     cy.location('pathname').should('include', '/search')

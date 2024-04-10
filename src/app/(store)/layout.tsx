b42ee3d6711dc2538575202a47c8c7897b91e70b
Gabriela França <gabifmouraroc@outlook.com>
2024-04-10 10:49:41 -0300

    configurações

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,13 +1,15 @@
 import { CartProvider } from '@/contexts/card-context'
 import { Header } from '@/components/header'
-import { ReactNode } from 'react'
+import { ReactNode, Suspense } from 'react'
 
 export default function StoreLayout({ children }: { children: ReactNode }) {
   return (
     <CartProvider>
-      <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-app gap-5 p-2 md:p-8">
-        <Header />
-        {children}
+      <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-app gap-5 px-8 py-8">
+        <Suspense>
+          <Header />
+          {children}
+        </Suspense>
       </div>
     </CartProvider>
   )
